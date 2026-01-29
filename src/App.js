6fef3f360d162d614641a6be071421c19c6a1d7c
mahenderreddym <mahender.mandhala@sedots.com>
2026-01-29 19:16:20 +0530

    todoApp-1st day push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (){
+
+    const [word,setWord] = useState('')
+
+    const [todo,setTodo]= useState([{
+        id:1,
+        name:'reddy'
+    }])
+
+
+    const onDeleteItem=(id)=>{
+
+        let newList = todo.filter((item)=>item.id!==id)
+        setTodo(newList)
+    }
+
+
+    const addToDo=()=>{
+let newTodo = {
+            id:Date.now(),
+            name:word
+         }
+
+        //  console.log(newTodo)
+        //  console.log("24",todo)
+        setTodo([...todo,newTodo])
+console.log(todo)
+        setWord('')
+    }
+
+    return (
+
+        <div>
+            <input type='text' value={word} style={{marginLeft:'40px'}} onChange={(e)=>setWord(e.target.value)}></input>
+  
+            <button  onClick={()=>addToDo()}>ADD TODO</button>
+
+{
+    todo.map((item)=>(
+        <ul key={item.id} >
+<li>{item.name}
+<button onClick={()=>onDeleteItem(item.id)}>Delete</button></li>
+        </ul>
+    ))
 }
 
-export default App;
+        </div>
+    )
+
+
+}
+
+
+export default App
